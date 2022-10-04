58465507d436601a2e34e10d64526d250323b3c3
yly <945635407@qq.com>
2022-10-04 02:47:31 +0000

    add autoencoder

---
+++ models/ae.py
@@ -0,0 +1,86 @@
+import torch.nn as nn
+from collections import OrderedDict
+
+
+class AutoEncoder(nn.Module):
+
+    def __init__(self, args):
+        super(AutoEncoder, self).__init__()
+        self.args = args
+        self.input_dim = args.ae_input_dim
+        self.output_dim = self.ae_input_dim
+        self.hidden_dims = args.ae_hidden_dims
+        self.hidden_dims.append(args.ae_latent_dim)
+        self.dims_list = (args.ae_hidden_dims +
+                          args.ae_hidden_dims[:-1][::-1])  # mirrored structure
+        self.n_layers = len(self.dims_list)
+        self.latent_dim = args.ae_latent_dim
+        self.n_clusters = args.n_clusters
+
+        # Validation check
+        assert self.n_layers % 2 > 0
+        assert self.dims_list[self.n_layers // 2] == self.latent_dim
+
+        # Encoder Network
+        layers = OrderedDict()
+        for idx, hidden_dim in enumerate(self.hidden_dims):
+            if idx == 0:
+                layers.update(
+                    {
+                        'linear0': nn.Linear(self.input_dim, hidden_dim),
+                        'activation0': nn.ReLU()
+                    }
+                )
+            else:
+                layers.update(
+                    {
+                        'linear{}'.format(idx): nn.Linear(
+                            self.hidden_dims[idx-1], hidden_dim),
+                        'activation{}'.format(idx): nn.ReLU(),
+                        'bn{}'.format(idx): nn.BatchNorm1d(
+                            self.hidden_dims[idx])
+                    }
+                )
+        self.encoder = nn.Sequential(layers)
+
+        # Decoder Network
+        layers = OrderedDict()
+        tmp_hidden_dims = self.hidden_dims[::-1]
+        for idx, hidden_dim in enumerate(tmp_hidden_dims):
+            if idx == len(tmp_hidden_dims) - 1:
+                layers.update(
+                    {
+                        'linear{}'.format(idx): nn.Linear(
+                            hidden_dim, self.output_dim),
+                    }
+                )
+            else:
+                layers.update(
+                    {
+                        'linear{}'.format(idx): nn.Linear(
+                            hidden_dim, tmp_hidden_dims[idx+1]),
+                        'activation{}'.format(idx): nn.ReLU(),
+                        'bn{}'.format(idx): nn.BatchNorm1d(
+                            tmp_hidden_dims[idx+1])
+                    }
+                )
+        self.decoder = nn.Sequential(layers)
+
+    def __repr__(self):
+        repr_str = '[Structure]: {}-'.format(self.input_dim)
+        for idx, dim in enumerate(self.dims_list):
+            repr_str += '{}-'.format(dim)
+        repr_str += str(self.output_dim) + '\n'
+        repr_str += '[n_layers]: {}'.format(self.n_layers) + '\n'
+        repr_str += '[n_clusters]: {}'.format(self.n_clusters) + '\n'
+        repr_str += '[input_dims]: {}'.format(self.input_dim)
+        return repr_str
+
+    def __str__(self):
+        return self.__repr__()
+
+    def forward(self, X, latent=False):
+        output = self.encoder(X)
+        if latent:
+            return output
+        return self.decoder(output)
--- templates.py
+++ templates.py
@@ -10,7 +10,58 @@ def set_template(args):
 
     if args.template is None:
         return
-    elif args.template.startswith('train_lsbert'):
+    elif args.template.startswith('train_bertls_clustered'):
+        args.mode = 'train'
+        args.rated = False
+
+        args.n_clusters = 2
+
+        args.dataset_code = 'oulad'
+        args.min_rating = 0
+        args.min_uc = 5
+        args.min_sc = 3
+        args.split = 'leave_one_out'
+
+        args.dataloader_code = 'bert'
+        batch = 128
+        args.train_batch_size = batch
+        args.val_batch_size = batch
+        args.test_batch_size = batch
+
+        args.train_negative_sampler_code = 'random'
+        args.train_negative_sample_size = 0
+        args.train_negative_sampling_seed = 0
+        args.test_negative_sampler_code = 'popular'
+        args.test_negative_sample_size = 100
+        args.test_negative_sampling_seed = 98765
+
+        args.trainer_code = 'bert_ls'
+        args.num_gpu = 1
+        args.device_idx = '0'
+        args.optimizer = 'Adam'
+        args.lr = 0.001
+        args.enable_lr_schedule = True
+        args.decay_step = 25
+        args.gamma = 1.0
+        args.num_epochs = 2
+        args.metric_ks = [1, 5, 10, 20, 50, 100]
+        args.best_metric = 'NDCG@10'
+
+        args.model_code = 'bert_ls'
+        args.model_init_seed = 0
+
+        args.bert_dropout = 0.1
+        args.bert_hidden_units = 256
+        args.bert_mask_prob = 0.15
+        args.bert_max_len = 100
+        args.bert_num_blocks = 2
+        args.bert_num_heads = 4
+
+        args.ae_input_dim = args.bert_hidden_units
+        args.ae_hidden_dims = [500, 500, 2000]
+        args.ae_latent_dim = 10
+
+    elif args.template.startswith('train_bertls_base'):
         args.mode = 'train'
         args.rated = False
 
@@ -42,7 +93,7 @@ def set_template(args):
         args.enable_lr_schedule = True
         args.decay_step = 25
         args.gamma = 1.0
-        args.num_epochs = 25
+        args.num_epochs = 2
         args.metric_ks = [1, 5, 10, 20, 50, 100]
         args.best_metric = 'NDCG@10'
 
